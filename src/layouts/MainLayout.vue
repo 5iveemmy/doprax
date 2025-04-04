94ee87628eccdc0475d7685f6f73b19438b036ef
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 12:11:47 +0100

    chore: create main layout component

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="bg-gray-50 px-4 py-8 md:px-8 md:py-8">
+    <slot />
+  </div>
+</template>
--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import Header from '../components/Header.vue'
+import MainLayout from '../layouts/MainLayout.vue'
 import StepperContainer from '../components/StepperContainer.vue'
 </script>
 
 <template>
-  <Header />
-  <div class="bg-gray-50 px-4 py-8 md:px-8 md:py-8">
+  <MainLayout>
     <StepperContainer />
-  </div>
+  </MainLayout>
 </template>
