eff3d693a4f480c4191cf73b6b1913ddeb4c314c
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 22:07:11 +0100

    chore: component clean up

--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import MainLayout from '../layouts/MainLayout.vue'
 import StepperContainer from '../components/StepperContainer.vue'
 </script>
 
 <template>
-  <MainLayout>
-    <StepperContainer />
-  </MainLayout>
+  <StepperContainer />
 </template>
