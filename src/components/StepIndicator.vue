eb7c31a596eb23e23f531f90f5f46888aa66e256
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 10:49:48 +0100

    chore: implement step indicator component

--- src/components/StepIndicator.vue
+++ src/components/StepIndicator.vue
@@ -6,13 +6,18 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex w-full gap-8">
+  <div class="flex w-full flex-col gap-4 md:flex-row md:gap-8">
     <div v-for="(step, index) in steps" :key="step.id" class="flex-1">
       <div
-        class="flex flex-col gap-1 border-t-4 pt-2"
-        :class="[index < currentStep ? 'border-primary-800' : 'border-gray-300']"
+        class="flex flex-col gap-1 border-t-0 border-l-4 pt-2 pb-2 pl-5 md:border-t-4 md:border-l-0 md:pt-5 md:pb-0 md:pl-0"
+        :class="[index < currentStep ? 'border-primary-800' : 'border-gray-200']"
       >
-        <h1 class="text-primary-800 pt-5 text-xs md:text-sm">Step {{ step.id }}</h1>
+        <h1
+          class="text-xs md:text-sm"
+          :class="[index < currentStep ? 'text-primary-800' : 'text-gray-500']"
+        >
+          Step {{ step.id }}
+        </h1>
         <h2 class="text-xs font-medium md:text-sm">{{ step.title }}</h2>
         <p class="text-xs text-gray-500 md:text-sm">{{ step.description }}</p>
       </div>
