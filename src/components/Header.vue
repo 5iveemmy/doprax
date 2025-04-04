278566e00302014cf68b458994f00e09c9bd0b36
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 10:46:21 +0100

    chore: implement header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-white px-8 py-4 drop-shadow-xs">
+  <div class="bg-white px-4 py-4 drop-shadow-xs md:px-8 md:py-5">
     <h1 class="font-semibold">Cloud Service Deployment</h1>
   </div>
 </template>
