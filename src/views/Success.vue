e073d1eb9297bd61c54c7fe413a01728dbf47a4f
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 22:07:24 +0100

    chore: add link to button

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -32,6 +32,7 @@
     </p>
 
     <button
+      @click="$router.push({ name: 'CloudServices' })"
       class="bg-primary-800 mt-6 cursor-pointer rounded-md px-4 py-3 text-xs text-white md:text-sm"
     >
       View cloud service
