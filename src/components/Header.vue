4ef80a83507fd1d3cf7c390669f5f7b08aaf9d08
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 22:05:19 +0100

    chore: change header text to a link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,8 @@
 
 <template>
   <div class="bg-white px-4 py-4 drop-shadow-xs md:px-8 md:py-5">
-    <h1 class="font-semibold">Cloud Service Deployment</h1>
+    <router-link to="/" class="cursor-pointer font-semibold">
+      <h1>Cloud Service Deployment</h1>
+    </router-link>
   </div>
 </template>
