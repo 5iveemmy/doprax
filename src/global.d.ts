91269e58974f9577e607a2da23ac0873acb7adeb
5iveemmy <ainemmanuel87@gmail.com>
2025-04-04 10:51:11 +0100

    chore: create global type file

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+interface Step {
+  id: number
+  title: string
+  description: string
+}
+
+interface Option {
+  id: string
+  name: string
+}
+
+interface InstanceTypes {
+  id: string
+  title: string
+  subtitle: string
+}
